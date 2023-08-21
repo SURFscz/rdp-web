f7b033e70b8d17d3f396a6af7c4074ef8db032f9
Harry Kodden <harry.kodden@surf.nl>
2023-08-21 22:50:03 +0200

    wip

--- client/html/index.php
+++ client/html/index.php
@@ -34,7 +34,7 @@ if (isset($_COOKIE['token'])) {
             <head>
                 <title>RDP-WEP</title>
                 <meta name="viewport" content="width=device-width, initial-scale=1">
-                <link rel="stylesheet" href="css/style.css">
+                <link rel="stylesheet" href="css/client.css">
             </head>
             <body>
                 <div>
@@ -160,123 +160,16 @@ if (isset($_COOKIE['token'])) {
     <head>
     <meta charset="UTF-8">
         <title>Desktop Server</title>
-        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/client.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     </head>
     <body>
-        <script src="https://cdn.bootcss.com/jquery/3.2.1/jquery.min.js"></script>
-        <script type="text/javascript" src="guacamole-common-js/all.min.js"></script>
+        <script type="text/javascript" src="https://cdn.bootcss.com/jquery/3.2.1/jquery.min.js"></script>
+        <script type="text/javascript" src="js/guacamole-common-js/all.min.js"></script>
+        <script type="text/javascript" src="js/client.js"></script>
 
         <div id="display" style="height:100vh;">
             <div id="connect" class="connect"/>
         </div>
-
-        <!-- Init -->
-        <script type="text/javascript">
-            var display = document.getElementById("display");
-          
-            let WIDTH = Math.round($(display).width())
-            let HEIGHT = Math.round($(display).height())
-
-            const Tunnel = new Guacamole.WebSocketTunnel("wss://<?php echo $_SERVER['URL_GUACD']; ?>");
-            const client = new Guacamole.Client(Tunnel);
-            
-            var connected = false;
-            var dis = client.getDisplay().getElement();
-            var context;
-
-            document.getElementById('connect').addEventListener('click', function() {
-                if (! connected) {
-                   client.connect('token=<?php echo base64_encode($json); ?>&width='+WIDTH+'&height='+HEIGHT+'&dpi=96&GUAC_AUDIO=audio/L8&GUAC_AUDIO=audio/L16');
-                }
-            });
-
-            client.onstatechange = function clientStateChanged(state) {
-                console.log("State change: "+state);
-                if (state === 3) {
-                    connected = true;
-
-                    console.log("Client is connected !")
-                    document.getElementById("connect").style.visibility="hidden"; 
-                }
-            };
-        
-            // Add client to display div
-            display.appendChild(dis);
-            
-            // Error handler
-            client.onerror = function(error) {
-                console.log(error);
-            };
-
-            // Connect
-            let clickHandler = function() {
-                if (! connected) 
-                   client.connect('token=<?php echo base64_encode($json); ?>&width='+WIDTH+'&height='+HEIGHT+'&dpi=96&GUAC_AUDIO=audio/L8&GUAC_AUDIO=audio/L16');
-            }
-
-            client.onaudio = function clientAudio(stream, mimetype) {
-                console.log("AUDIO: "+mimetype)
-                context = Guacamole.AudioContextFactory.getAudioContext();
-                context.resume().then(() => console.log('play audio'));
-            }
-
-            // Window resize...
-            window.onresize = function () {
-                let display = document.getElementById("display");
-                let W = Math.round($(display).width())
-                let H = Math.round($(display).height())
-
-                Tunnel.sendMessage('size', W, H);
-            }
-
-            // Disconnect on close
-            window.onunload = function() {
-                if (connected) {
-                   client.disconnect();
-                }
-
-                document.getElementById("connect").style.visibility="visible"; 
-                connected = false;
-            }            
-        </script>
-        <script type="text/javascript">
-           
-            dis = client.getDisplay();
-            function sendScaledMouseState(mouseState) {
-                var scaledState = new Guacamole.Mouse.State(
-                    mouseState.x / dis.getScale(),
-                    mouseState.y / dis.getScale(),
-                    mouseState.left,
-                    mouseState.middle,
-                    mouseState.right,
-                    mouseState.up,
-                    mouseState.down
-                );
-                client.sendMouseState(scaledState);
-            }
-          
-            // Mouse
-            var mouse = new Guacamole.Mouse(client.getDisplay().getElement());
-            
-            mouse.onmousedown = 
-            mouse.onmouseup   =
-            mouse.onmousemove = function(mouseState) {
-                sendScaledMouseState(mouseState);
-                dis.showCursor(false);
-            };
-
-            // Keyboard
-            var keyboard = new Guacamole.Keyboard(document);
-
-            keyboard.onkeydown = function (keysym) {
-                client.sendKeyEvent(1, keysym);
-            };
-
-            keyboard.onkeyup = function (keysym) {
-                client.sendKeyEvent(0, keysym);
-            };
-
-        </script>
     </body>
 </html>
